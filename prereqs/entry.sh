7441ff7dce3455b2cfec829f4c43f5ac2a1aecdd
Vitaly Petrov <93202696+vitalypetrovisbest@users.noreply.github.com>
2021-11-25 03:45:18 +0200

    adding Dockerfile and prerequisites needed to build fbrat inside of Docker

---
+++ prereqs/entry.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+N_THREADS=$(nproc --all --ignore=2)
+mkdir -p ${FBRAT_WORKING_DIR}/build
+cd ${FBRAT_WORKING_DIR}/build
+cmake3 -DCMAKE_INSTALL_PREFIX=${FBRAT_WORKING_DIR}/build/testroot \
+    -DCMAKE_PREFIX_PATH=${FBRAT_WORKING_DIR}/build/testroot -DAPIDOC_INSTALL_PATH=${APIDOCDIR} \
+    -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
+    -DBUILD_WITH_COVERAGE=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja ..
+cd ${FBRAT_WORKING_DIR}/build
+ninja-build -j$N_THREADS
+ninja-build install -j$N_THREADS
