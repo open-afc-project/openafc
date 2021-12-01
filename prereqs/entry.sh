8e712070bc3fc421ec8947ba9b7aa0ef36ad2520
Dima Rozenfeld <dima.rozenfeld@broadcom.com>
2021-12-01 05:36:31 -0800

    add env optional valiables that can de provided to docker run command to have flaxible source dir path both options work: docker run --rm -it --user `id -u`:`id -g` --group-add `id -G | sed "s/ / --group-add /g"` -v \ /projects/wcc_wlansw_01:/wd/fbrat -e AFC_SUFX=work/dimar/github/open-afc fbrat-ndrv
    docker run --rm -it --user `id -u`:`id -g` --group-add `id -G | sed "s/ / --group-add /g"` -v `pwd`:/wd/fbrat fbrat-ndrv

--- prereqs/entry.sh
+++ prereqs/entry.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 N_THREADS=$(nproc --all --ignore=2)
-mkdir -p ${FBRAT_WORKING_DIR}/${AFC_SUFX}/build
-cd ${FBRAT_WORKING_DIR}/${AFC_SUFX}/build
-cmake3 -DCMAKE_INSTALL_PREFIX=${FBRAT_WORKING_DIR}/${AFC_SUFX}/build/testroot \
-    -DCMAKE_PREFIX_PATH=${FBRAT_WORKING_DIR}/${AFC_SUFX}/build/testroot -DAPIDOC_INSTALL_PATH=${FBRAT_WORKING_DIR}/${AFC_SUFX}/build/testroot/share/doc/fbrat-api \
+FBRAT_WORKING_DIR=/wd/fbrat/${AFC_SUFX}
+APIDOCDIR=${FBRAT_WORKING_DIR}/build/testroot/share/doc/fbrat-api
+mkdir -p ${FBRAT_WORKING_DIR}/build
+cd ${FBRAT_WORKING_DIR}/build
+cmake3 -DCMAKE_INSTALL_PREFIX=${FBRAT_WORKING_DIR}/build/testroot \
+    -DCMAKE_PREFIX_PATH=${FBRAT_WORKING_DIR}/build/testroot -DAPIDOC_INSTALL_PATH=${FBRAT_WORKING_DIR}/build/testroot/share/doc/fbrat-api \
     -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
     -DBUILD_WITH_COVERAGE=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja ..
-cd ${FBRAT_WORKING_DIR}/${AFC_SUFX}/build
+cd ${FBRAT_WORKING_DIR}/build
 ninja-build -j$N_THREADS
 ninja-build install -j$N_THREADS
