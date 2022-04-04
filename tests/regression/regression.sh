36b08c1a47af58430f233e0b8b1dd3c703eafaf1
Dima Rozenfeld <dima.rozenfeld@broadcom.com>
2022-04-04 04:55:44 -0700

    OA-145 update  git hash

--- tests/regression/regression.sh
+++ tests/regression/regression.sh
@@ -125,7 +125,8 @@ build_dev_server() {
   (trap 'kill 0' SIGINT; docker_build_and_push ${wd}/dockerfiles/Dockerfile-openafc-centos-preinstall-image ${PRINST_DEV}:${rand} & docker_build_and_push ${wd}/dockerfiles/Dockerfile-for-build ${D4B_DEV}:${rand})
   EXT_ARGS="--build-arg BLD_TAG=${rand} --build-arg PRINST_TAG=${rand} --build-arg BLD_NAME=${D4B_DEV} --build-arg PRINST_NAME=${PRINST_DEV}"
   docker_build_and_push ${wd}/Dockerfile ${SRV_DI}:${rand} "${EXT_ARGS}"
-  docker_build ${wd}/Dockerfile ${SRV_DI}:${rand} "--build-arg BLD_TAG=${rand} --build-arg PRINST_TAG=${rand} --build-arg BLD_NAME=${D4B_DEV} --build-arg PRINST_NAME=${PRINST_DEV} --build-arg BUILDREV=`git rev-parse --short HEAD`"
+  BUILDREV=`git rev-parse --short HEAD`
+  docker_build ${wd}/Dockerfile ${SRV_DI}:${rand} "--build-arg BLD_TAG=${rand} --build-arg PRINST_TAG=${rand} --build-arg BLD_NAME=${D4B_DEV} --build-arg PRINST_NAME=${PRINST_DEV} --build-arg BUILDREV=${BUILDREV}"
 }
 
 build_dev_server $1 $2
