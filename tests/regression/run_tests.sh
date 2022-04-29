9bdcefba5ca6efc5110b863e0db4a955c93f6099
Dima Rozenfeld <93186435+dr942120@users.noreply.github.com>
2022-04-30 01:09:52 +0300

    Oa 210 CI regression  test time optimizations (#134)
    * OA-210 regr test time optimizations
    
    * OA-210 add script that runs regs tests as subtasks
    
    * OA-210 EOL
    
    * OA-210 fix provide srvr addr
    
    * OA-210 run regr on github actions server
    
    * OA-210 update regr test script
    
    * OA-210 update regr test script
    
    * OA-210 update regr test script
    
    * OA-210 update regr test script
    
    * Update regressions-test-remote.yml
    
    OA-210 fix scrip exec command
    
    * Update run_tests.sh
    
    OA-210 remove dbg print
    
    * OA-210 cleanup regr test script
    
    * OA-210 dbg
    
    * OA-210 remove dbg
    
    * OA-210 remove dbg
    
    * OA-210 fix char

---
+++ tests/regression/run_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright © 2022 Broadcom. All rights reserved. The term "Broadcom"
+# refers solely to the Broadcom Inc. corporate affiliate that owns
+# the software below. This work is licensed under the OpenAFC Project License,
+# a copy of which is included with this software program
+#
+trap 'kill 0' SIGINT
+echo `pwd`
+wd=${1}
+di_name=${2}
+addr=${3}
+port=${4:-443}
+
+loop() {
+    start=0
+    max=${1}
+    step=${2:-10}
+    s=${start}
+    while [ $s -le $max ]
+    do
+        e=$(($((s + ${step}))<=${max} ? $((s + ${step})) : ${max}))
+        echo "from $s  to $e"
+        # run processes and store pids in array
+        for i in `seq $((s+1)) ${e}`; do
+            docker run --rm ${di_name} --cfg addr=${addr} port=${port} --test r=${i} &
+            pids+=( $! )
+        done
+        s=$((s + ${step}))
+        # wait for all pids
+        for pid in ${pids[*]}; do
+            wait $pid
+        done
+        unset pids
+    done
+}
+
+cd $wd/tests
+loop 52 20
+
+
+# Local Variables:
+# vim: sw=2:et:tw=80:cc=+1
