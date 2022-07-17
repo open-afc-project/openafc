9b97b8748baa6089c9de844292b4cd558bd50f46
Dima Rozenfeld <93186435+dr942120@users.noreply.github.com>
2022-07-17 15:36:44 +0300

    OA-307 build and push object storage docker image (#208)
    * OA-307 build and push object storage docker image
    
    * --precision and field-level difference report added to afc_tests.py
    
    * OA-307 use TIP's openafc docker images repo instead of dr942120 dockerhub repo
    
    * fix infra srv_di build
    
    * rebase
    
    * OA-307 use TIP's openafc docker images repo instead of dr942120 dockerhub repo
    
    * OA-307 use TIP's opeafc docker images repo instead of dr942120 dockerhub for server image
    
    Co-authored-by: fs936724 <fs936724@broadcom.com>
    Co-authored-by: vitalypetrovisbest <93202696+vitalypetrovisbest@users.noreply.github.com>

--- tests/regression/regression.sh
+++ tests/regression/regression.sh
@@ -8,41 +8,10 @@
 
 set -x
 
-# 1.1 start new instances for build and dev_srvr on AWS (afc-build,afc-dev )
-
-# BUILD Images
-# 1.2 ssh afc-build: git clone test_branch
-# 1.3 ssh afc-build: build_and_push d4b_dev, build_and_push preinst_dev (in parallel)
-# 1.4 ssh afc-build: buid_and_push srvr_di (d4b_dev, preinst_dev)
-# 1.5 ssh afc-dev: reload srv_di
-
-# RUN TESTS
-# 1.6 githubA:  run_regression on afc-dev
-
-# IF PASSED
-# 1.7 ssh afc-build: push d4b_dev:latest, d4b_dev:GitHash to docker_hub_repo
-# 1.8 ssh afc-build: push preinst_dev:latest, preinst_dev:GitHash to docker_hub_repo
-# 1.9 Ssh afc-build: push srvr_di:latest,srvr_di:latest:GitHash to the repo (for prod)
-
-# 1.10 cleanup
-#   remove from docker: repo d4b_dev, preinst_dev, srvr_di
-#   kill (afc-build)
-#   kill (afc-dev)
-#   rm test branch
-
-#    * build_and_push - pushes docker image to dev_test repo for tests
-#    * afc-build, afc0-dev can be the same or different.
-#    * afc-build can be different at every step
-#    * d4b_dev - Docker-for-Build
-#    * preinst_dev - centos-preinstall image
-#    * srv_di - OpenAFC server docker image with latest changes
-
-
-# 1.2 ssh afc-build: git clone test_branch
-
-PRINST_DEV="dr942120/centos-preinstall-image" # Dockerfile-openafc-centos-preinstall dev image name
-D4B_DEV="dr942120/centos-build-image"         # Dockerfile-for-build dev image name
-SRV_DI="dr942120/srv_di"                   # Dockerfile dev image name
+PRINST_DEV="public.ecr.aws/w9v6y1o0/openafc/centos-preinstall-image" # preinst image name
+D4B_DEV="public.ecr.aws/w9v6y1o0/openafc/centos-build-image"         # dev image name
+SRV_DI="110738915961.dkr.ecr.us-east-1.amazonaws.com/afc-server"     # server image
+OBJST_DI="public.ecr.aws/w9v6y1o0/openafc/objstorage-image"          # object storage
 # ADDR="git@github.com:Telecominfraproject/open-afc.git"
 
 
@@ -96,20 +65,6 @@ docker_build_and_push() {
   fi
 }
 
-docker_build_and_push_server () {
-  file=${1}         # Name of the Dockerfile
-  image=${2}        # Name and optionally a tag in the 'name:tag' format
-  d4b_tag=${3}      # base docker_for_build tag
-  preinst_tag=${4}  # base preinst image tag
-  d4b_name=${5}     # base docker_for_build image name
-  preinst_name=${6} # base preinst image name
-
-  docker_build ${file} ${image} "--build-arg BLD_TAG=${d4b_tag} --build-arg PRINST_TAG=${preinst_tag} --build-arg BLD_NAME=${d4b_name} --build-arg PRINST_NAME=${preinst_name}"
-  if [ $? -eq 0 ]; then
-    docker_push ${image}
-  fi
-}
-
 git_clone() {
   addr=${1}
   wd=${2}
@@ -120,13 +75,16 @@ git_clone() {
 
 build_dev_server() {
   wd=${1}
-  rand=${2}
+  tag=${2}
   cd ${wd}
+
+  aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/w9v6y1o0
   BUILDREV=`git rev-parse --short HEAD`
-  (trap 'kill 0' SIGINT; docker_build_and_push ${wd}/dockerfiles/Dockerfile-openafc-centos-preinstall-image ${PRINST_DEV}:${rand} & docker_build_and_push ${wd}/dockerfiles/Dockerfile-for-build ${D4B_DEV}:${rand})
-  EXT_ARGS="--build-arg BLD_TAG=${rand} --build-arg PRINST_TAG=${rand} --build-arg BLD_NAME=${D4B_DEV} --build-arg PRINST_NAME=${PRINST_DEV} --build-arg BUILDREV=${BUILDREV}"
-  docker_build_and_push ${wd}/Dockerfile ${SRV_DI}:${rand} "${EXT_ARGS}"
-#  docker_build ${wd}/Dockerfile ${SRV_DI}:${rand} "--build-arg BLD_TAG=${rand} --build-arg PRINST_TAG=${rand} --build-arg BLD_NAME=${D4B_DEV} --build-arg PRINST_NAME=${PRINST_DEV} --build-arg BUILDREV=${BUILDREV}"
+  (trap 'kill 0' SIGINT; docker_build_and_push ${wd}/dockerfiles/Dockerfile-openafc-centos-preinstall-image ${PRINST_DEV}:${tag} & docker_build_and_push ${wd}/dockerfiles/Dockerfile-for-build ${D4B_DEV}:${tag})
+  cd ${wd}/src/filestorage && docker_build_and_push Dockerfile ${OBJST_DI}:${tag} ; cd ${wd}
+  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 110738915961.dkr.ecr.us-east-1.amazonaws.com
+  EXT_ARGS="--build-arg BLD_TAG=${tag} --build-arg PRINST_TAG=${tag} --build-arg BLD_NAME=${D4B_DEV} --build-arg PRINST_NAME=${PRINST_DEV} --build-arg BUILDREV=${BUILDREV}"
+  docker_build_and_push ${wd}/Dockerfile ${SRV_DI}:${tag} "${EXT_ARGS}"
 }
 
 build_dev_server $1 $2
