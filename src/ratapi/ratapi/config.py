bf379dbfb9adfb5491919179afa111cb3e19e450
huy ton <huy.ton@broadcom.com>
2022-09-06 14:17:12 -0700

    Update config.py and priv_config.py to clearly separate default configuration

--- src/ratapi/ratapi/config.py
+++ src/ratapi/ratapi/config.py
@@ -59,6 +59,3 @@ DAILY_ULS_RAN_TODAY = False
 
 # OIDC default configurations
 OIDC_LOGIN = False
-OIDC_CLIENT_ID='547511594635-h541g370p903uf082p42r9ic34qun5eb.apps.googleusercontent.com'
-OIDC_CLIENT_SECRET='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'
-OIDC_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
---
+++ src/ratapi/ratapi/priv_config.py
@@ -0,0 +1,10 @@
+''' Application private configuration data.
+'''
+
+# Example OIDC configuration.  Replace with proper values for your 
+# OIDC identity server
+#
+# OIDC_LOGIN = True
+# OIDC_CLIENT_ID='547511594635-h541g370p903uf082p42r9ic34qun5eb.apps.googleusercontent.com'
+# OIDC_CLIENT_SECRET='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'
+# OIDC_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
