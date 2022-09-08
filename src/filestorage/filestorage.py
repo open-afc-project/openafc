e22c486d63f791bd811b8f7ebf7abb80c88e2615
yura.dinkevich <yura.dinkevich@broadcom.com>
2022-09-08 15:40:25 +0300

    OA-382 Google Storage objserver support

--- src/filestorage/filestorage.py
+++ src/filestorage/filestorage.py
@@ -13,10 +13,15 @@ Provides HTTP server for file exchange between Celery clients and workers.
 import os
 import errno
 import re
+import io
 from flask import Flask, request, helpers, abort, make_response
 import logging
 import shutil
 from werkzeug.utils import secure_filename
+import abc
+import google.cloud.storage
+
+NET_TIMEOUT = 600 # The amount of time, in seconds, to wait for the server response
 
 flask = Flask(__name__)
 flask.config.from_pyfile('filestorage_config.py')
@@ -30,6 +35,11 @@ elif flask.config['LOG_FILE']:
 else:
     logging.basicConfig(level=flask.config['LOG_LEVEL'])
 
+if flask.config["OBJSTORAGE"] == "GoogleCloudBucket":
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = flask.config["GOOGLE_CLOUD_CREDENTIALS_JSON"]
+    client = google.cloud.storage.client.Client()
+    bucket = client.bucket(flask.config["GOOGLE_CLOUD_BUCKET"])
+
 loc = {
     "pro": flask.config['PRO_LOCATION'],
     "cfg": flask.config['CFG_LOCATION'],
@@ -42,6 +52,110 @@ loc = {
 #    path = os.path.abspath(path)
 #    return pattern.match(path)
 
+class ObjInt:
+    """ Abstract class for data prot operations """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, file_name):
+        self._file_name = file_name
+
+    @abc.abstractmethod
+    def write(self, data):
+        pass
+
+    @abc.abstractmethod
+    def read(self):
+        pass
+
+    @abc.abstractmethod
+    def head(self):
+        pass
+
+    @abc.abstractmethod
+    def delete(self):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        pass
+
+
+class ObjIntLocalFS(ObjInt):
+    def write(self, data):
+        self.__mkdir_local(os.path.dirname(self._file_name))
+        with open(self._file_name, 'wb') as f:
+            f.write(data)
+
+    def read(self):
+        if not os.path.isfile(self._file_name):
+            return None
+        with open(self._file_name) as hfile:
+            return hfile.read()
+
+    def head(self):
+        return os.path.exists(self._file_name)
+
+    def delete(self):
+        if os.path.exists(self._file_name):
+            if os.path.isdir(self._file_name):
+                shutil.rmtree(self._file_name)
+            else:
+                os.remove(self._file_name)
+
+    def __mkdir_local(self, path):
+        flask.logger.debug('__mkdir_local({})'.format(path))
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                flask.logger.error('mkdir_local: something wrong {}'.format(path))
+                raise
+
+
+class ObjIntGoogleCloudBucket(ObjInt):
+    def write(self, data):
+        blob = bucket.blob(self._file_name)
+        blob.upload_from_string(data,
+                                content_type="application/octet-stream",
+                                timeout=NET_TIMEOUT)
+
+    def read(self):
+        blob = bucket.blob(self._file_name)
+        return blob.download_as_bytes(raw_download=True,
+                                      timeout=NET_TIMEOUT)
+
+    def head(self):
+        blobs = client.list_blobs(bucket,
+                                  prefix=self._file_name,
+                                  delimeter = "/",
+                                  timeout=NET_TIMEOUT)
+        return blobs is not None
+
+    def delete(self):
+        blobs = client.list_blobs(bucket,
+                                  prefix=self._file_name,
+                                  delimeter = "/",
+                                  timeout=NET_TIMEOUT)
+        for blob in blobs:
+            try:
+                blob.delete(timeout=NET_TIMEOUT)
+            except:
+                pass # ignore google.cloud.exceptions.NotFound
+
+
+class Objstorage:
+    def open(self, name):
+        """ Create ObjInt instance """
+        flask.logger.debug("Objstorage.open({})".format(name))
+        if flask.config["OBJSTORAGE"] == "GoogleCloudBucket":
+            return ObjIntGoogleCloudBucket(name)
+        if flask.config["OBJSTORAGE"] == "LocalFS":
+            return ObjIntLocalFS(name)
+        raise Exception("Unsupported OBJSTORAGE \"{}\"".format(flask.config["OBJSTORAGE"]))
+
+
 def get_local_path(path):
     prefix = path.split('/')[0]
     if prefix not in loc:
@@ -51,37 +165,34 @@ def get_local_path(path):
     return path
 
 
-def mkdir_local(path):
-    flask.logger.debug('mkdir_local path={}'.format(path))
-    try:
-        os.makedirs(path)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            flask.logger.error('mkdir_local: something wrong {}'.format(path))
-            raise
-
-
 @flask.route('/'+'<path:path>', methods=['POST'])
 def post(path):
     ''' File upload handler. '''
-    flask.logger.debug('post method={}, path={}'.format(request.method, path))
-    path = get_local_path(path)
+    try:
+        flask.logger.debug('post method={}, path={}'.format(request.method, path))
+        path = get_local_path(path)
 
-    if 'file' in request.files:
-        if not request.files['file']:
-            flask.logger.error('No file in request')
-            abort(422, 'Unprocessable Entity')
-        if request.files['file'].filename == '':
-            flask.logger.error('Empty filename')
-            abort(422, 'Unprocessable Entity')
-        path = os.path.join(path, request.files['file'].filename)
-        mkdir_local(os.path.dirname(path))
-        request.files['file'].save(path)
-        return make_response('OK', 200)
-    mkdir_local(os.path.dirname(path))
-    with open(path, 'wb') as f:
-        f.write(request.get_data())
-    flask.logger.debug('POST path {}'.format(path))
+        data = None
+
+        if 'file' in request.files:
+            if not request.files['file']:
+                flask.logger.error('No file in request')
+                abort(400)
+            if request.files['file'].filename == '':
+                flask.logger.error('Empty filename')
+                abort(400)
+            path = os.path.join(path, request.files['file'].filename)
+            data = request.files['file'].read()
+        else:
+            data = request.get_data()
+
+        objst = Objstorage()
+        with objst.open(path) as hobj:
+            hobj.write(data)
+    except Exception as e:
+        flask.logger.error(e)
+        return abort(500)
+
     return make_response('OK', 200)
 
 
@@ -91,12 +202,14 @@ def delete(path):
     flask.logger.debug('delete method={}, path={}'.format(request.method, path))
     path = get_local_path(path)
 
-    if os.path.exists(path):
-        if os.path.isdir(path):
-            shutil.rmtree(path)
-        else:
-            os.remove(path)
-        flask.logger.debug('DELETE {}'.format(path))
+    try:
+        objst = Objstorage()
+        with objst.open(path) as hobj:
+            hobj.delete()
+    except Exception as e:
+        flask.logger.error(e)
+        return abort(500)
+
     return make_response('OK', 204)
 
 
@@ -106,9 +219,16 @@ def head(path):
     flask.logger.debug('head method={}, path={}'.format(request.method, path))
     path = get_local_path(path)
 
-    if not os.path.exists(path):
-        abort(404, 'File not found.')
-    return make_response('OK', 200)
+    try:
+        objst = Objstorage()
+        with objst.open(path) as hobj:
+            if hobj.head():
+                return make_response('OK', 200)
+            else:
+                abort(404)
+    except Exception as e:
+        flask.logger.error(e)
+        return abort(500)
 
 
 @flask.route('/'+'<path:path>', methods=['GET'])  # handle URL with filename
@@ -117,19 +237,25 @@ def get(path):
     flask.logger.debug('get method={}, path={}'.format(request.method, path))
     path = get_local_path(path)
 
-    if not os.path.exists(path):
-        flask.logger.error('{}: File not found'.format(path))
-        abort(404, 'File not found.')
-    flask.logger.debug('GET {}'.format(path))
-    if os.path.isdir(path):
-        return make_response('OK', 204)
-    return helpers.send_file(path)
+    try:
+        objst = Objstorage()
+        with objst.open(path) as hobj:
+            data = hobj.read()
+            if data:
+                return helpers.send_file(
+                    io.BytesIO(data),
+                    mimetype='application/octet-stream')
+            else:
+                flask.logger.error('{}: File not found'.format(path))
+                abort(404)
+    except Exception as e:
+        flask.logger.error(e)
+        return abort(500)
 
 
 if __name__ == '__main__':
-    flask.logger.debug("host={} port={} FILE_LOCATION={}".format(flask.config['SERVER_HOST'], flask.config['SERVER_PORT'], flask.config['FILE_LOCATION']))
+    flask.logger.debug("host={} port={} FILE_LOCATION={} OBJSTORAGE={}".format(flask.config['SERVER_HOST'], flask.config['SERVER_PORT'], flask.config['FILE_LOCATION'], flask.config["OBJSTORAGE"]))
 
-    mkdir_local(flask.config['FILE_LOCATION'])
     # production env:
     import waitress
     waitress.serve(flask, host=flask.config['SERVER_HOST'],
