d232555519c162d28256e36e4648f94cbcef7f15
yura.dinkevich <yura.dinkevich@broadcom.com>
2022-11-13 19:23:34 +0200

    Fix progress.txt

--- prereqs/docker-entrypoint.sh
+++ prereqs/docker-entrypoint.sh
@@ -18,24 +18,15 @@ if [ "$BROKER_TYPE" == "internal" ]; then
 fi
 
 #celery
-DISABLE_HTTPD=${DISABLE_HTTPD:=no}
-CELERY_TYPE=${CELERY_TYPE:=internal}
-if [ "$CELERY_TYPE" == "internal" ]; then
-	CELERY_LOG=${CELERY_LOG:=INFO}
-	if [ "$DISABLE_HTTPD" == "no" ]; then
-		CELERY_OPTIONS=${CELERY_OPTIONS:="rat_1 rat_2 rat_3 rat_4 rat_5 rat_6 rat_7 rat_8 rat_9 rat_10"}
-		/bin/celery multi start $CELERY_OPTIONS -A ratapi.tasks.afc_worker --pidfile=/var/run/celery/%n.pid --logfile=/proc/self/fd/2 --loglevel=$CELERY_LOG &
-	else
-		CELERY_OPTIONS=${CELERY_OPTIONS:="rat_1 rat_2 rat_3 rat_4 rat_5 rat_6 rat_7 rat_8 rat_9 rat_10"}
-		/bin/celery multi start $CELERY_OPTIONS -A ratapi.tasks.afc_worker --pidfile=/var/run/celery/%n.pid --logfile=/proc/self/fd/2 --loglevel=$CELERY_LOG
-		sleep infinity
-	fi
-fi
-if [ "$DISABLE_HTTPD" == "no" ]; then
-	# apache
-	HTTPD_OPTIONS=${HTTPD_OPTIONS}
-	echo "/usr/sbin/httpd $HTTPD_OPTIONS -DFOREGROUND >"
-	/usr/sbin/httpd $HTTPD_OPTIONS -DFOREGROUND &
-	wait
-	exit $?
-fi
+CELERY_OPTIONS=${CELERY_OPTIONS:="rat_1 rat_2 rat_3 rat_4 rat_5 rat_6 rat_7 rat_8 rat_9 rat_10"}
+CELERY_LOG=${CELERY_LOG:=INFO}
+/bin/celery multi start $CELERY_OPTIONS -A ratapi.tasks.afc_worker --pidfile=/var/run/celery/%n.pid --logfile=/proc/self/fd/2 --loglevel=$CELERY_LOG &
+
+# apache
+HTTPD_OPTIONS=${HTTPD_OPTIONS}
+echo "/usr/sbin/httpd $HTTPD_OPTIONS -DFOREGROUND >"
+/usr/sbin/httpd $HTTPD_OPTIONS -DFOREGROUND &
+
+wait
+
+exit $?
