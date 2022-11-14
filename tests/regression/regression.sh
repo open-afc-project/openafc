cea344263d1375016d53870f9c44c449ac2c916a
Dima Rozenfeld <93186435+dr942120@users.noreply.github.com>
2022-11-14 10:19:33 +0200

    OA-435 use msghnd in test infra

--- tests/regression/regression.sh
+++ tests/regression/regression.sh
@@ -88,7 +88,8 @@ build_dev_server() {
   # cd to a work dir
   cd ${wd}
   # get last git commit hash number
-  BUILDREV=`git rev-parse --short HEAD`
+  # BUILDREV=`git rev-parse --short HEAD`
+  BUILDREV=oa435
 
   if [ ${push} -eq 1 ]; then
     docker_login
