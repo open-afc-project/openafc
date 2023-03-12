ad0e72b6a152bc02ca3b3134937c2f5e530ea218
yura.dinkevich <yura.dinkevich@broadcom.com>
2023-03-12 15:49:21 +0200

    OA-592 postgres version as script param

---
+++ tools/db_tools/update_db.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ $# -ne 4 ] || ! [ -d $1 ]; then
+	echo "Usage: $0 <pgdata_dir> <postgres_password> <old_postgres_version> <new_postgres_version>"
+	exit
+fi
+
+pgdata=$(readlink -f $1)
+pass=$2
+db_name=fbrat
+ver_old=$3
+ver_new=$4
+wd=$(dirname "$pgdata")
+
+mkdir -p "$wd"/dbtmp
+chmod 777 "$wd"/dbtmp
+
+docker pull postgres:$ver_old
+container=$(docker run --rm -d -e POSTGRES_PASSWORD="$pass" -e PGDATA=/var/lib/pgsql/data -e POSTGRES_DB="$db_name" -v "$pgdata":/var/lib/pgsql/data --name postgres_old postgres:$ver_old)
+sleep 3
+docker exec -it $container pg_dumpall -U postgres > "$wd"/dbtmp/dump.sql
+docker stop postgres_old
+
+mv "$pgdata" "$pgdata".back
+mkdir "$wd"/pgdata
+chmod 777 "$wd"/pgdata
+
+docker pull postgres:$ver_new
+container=$(docker run --rm -d -e POSTGRES_PASSWORD="$pass" -e PGDATA=/var/lib/pgsql/data -e POSTGRES_DB="$db_name" -v "$pgdata":/var/lib/pgsql/data -v "$wd"/dbtmp:/dbtmp --name postgres_new postgres:$ver_new)
+sleep 3
+docker exec -it $container psql -U postgres -f /dbtmp/dump.sql
+docker stop postgres_new
+
+rm -rf "$wd"/dbtmp
+
+
+
+
+
+
