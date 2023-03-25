817b1649e5049f12763750b4ef9f538339d28bda
yura.dinkevich <yura.dinkevich@broadcom.com>
2023-03-25 13:18:34 +0300

    OA-592 change password autorization

--- tools/db_tools/update_db.sh
+++ tools/db_tools/update_db.sh
@@ -15,23 +15,27 @@ ver_new=$4
 wd=$(dirname "$pgdata")
 
 mkdir -p "$wd"/dbtmp
-chmod 777 "$wd"/dbtmp
 
 docker pull postgres:$ver_old
 container=$(docker run --rm -d -e POSTGRES_PASSWORD="$pass" -e PGDATA=/var/lib/pgsql/data -e POSTGRES_DB="$db_name" -v "$pgdata":/var/lib/pgsql/data --name postgres_old postgres:$ver_old)
+docker logs -f postgres_old &
 sleep 3
+docker exec -it $container chown postgres:postgres /var/lib/pgsql/data -R
 docker exec -it $container pg_dumpall -U postgres > "$wd"/dbtmp/dump.sql
 docker stop postgres_old
 
 mv "$pgdata" "$pgdata".back
 mkdir "$wd"/pgdata
-chmod 777 "$wd"/pgdata
 
 docker pull postgres:$ver_new
 container=$(docker run --rm -d -e POSTGRES_PASSWORD="$pass" -e PGDATA=/var/lib/pgsql/data -e POSTGRES_DB="$db_name" -v "$pgdata":/var/lib/pgsql/data -v "$wd"/dbtmp:/dbtmp --name postgres_new postgres:$ver_new)
+docker logs -f postgres_new &
 sleep 3
+docker exec -it $container chown postgres:postgres /var/lib/pgsql/data -R
 docker exec -it $container psql -U postgres -f /dbtmp/dump.sql
 docker stop postgres_new
+
+sed -i "s/scram-sha-256$/trust/" "$pgdata"/pg_hba.conf
 
 rm -rf "$wd"/dbtmp
 
