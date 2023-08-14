b8ad55677da8ee3a071b05c9cf0326722dba02cd
Dima Rozenfeld <93186435+dr942120@users.noreply.github.com>
2023-08-14 14:15:27 +0300

    OA-792-rabbitmq-rcache-rmq-user-and-host-configuration-for-rcache (#623)

--- tests/regression/run_srvr.sh
+++ tests/regression/run_srvr.sh
@@ -7,7 +7,7 @@
 #
 set -x
 # ./run_srvr.sh /work/open-afc MY_TAG 0
-# 1. starts AFC server server 
+# 1. starts AFC server server
 # 2. export configuration form test database ( users, APs, afc-config)
 # 3. integrates the settings from #2 into the AFC server to prepare it to autiomation tests
 
@@ -15,7 +15,7 @@ wd=${1}         # full path to the afc project path
 TAG=${2}        # docker images tag
 randdir=${3:-0} # whether to use a dir with random name that includes tag name.
                 # like $wd/tests/regression_${TAG} If so, it should be already existing
-                # Usually used by regression test infra. not requred for user's unit testing    
+                # Usually used by regression test infra. not requred for user's unit testing
 export TAG
 
 source $wd/tests/regression/regression.sh
@@ -32,10 +32,10 @@ fi
 # go to test dir
 cd $tests_dir
 
-# create pipe and afc_config dir if does not exist 
+# create pipe and afc_config dir if does not exist
 [ ! -d pipe ] && mkdir pipe
 
-# export configuration from regression test container 
+# export configuration from regression test container
 docker run --rm -v `pwd`/pipe:/pipe ${RTEST_DI}:${TAG} --cmd exp_adm_cfg --outfile /pipe/export_admin_cfg.json
 check_ret $?
 # copy regr server tls/mtls config (if existing)
@@ -58,10 +58,6 @@ docker-compose exec -T rat_server rat-manage-api cert_id create  --location 3 \
 --cert_id FsDownloaderCertIdCA --ruleset_id CA_RES_DBS-06
 docker-compose exec -T rat_server rat-manage-api cert_id create  --location 3 \
 --cert_id FsDownloaderCertIdBR --ruleset_id BRAZIL_RULESETID
-docker-compose exec -T rmq rabbitmqctl add_user rcache rcache
-docker-compose exec -T rmq rabbitmqctl add_vhost rcache
-docker-compose exec -T rmq rabbitmqctl set_permissions -p rcache rcache ".*" ".*" ".*"
-
 
 # Local Variables:
 # vim: sw=2:et:tw=80:cc=+1
