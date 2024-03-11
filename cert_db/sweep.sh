edcb4dc707019b7ea26dc54ed5d552624bd06ded
huyton99 <104873772+huyton99@users.noreply.github.com>
2024-03-11 00:47:38 -0700

    831: add copyrights for cert_db & trial tools (#832)

--- cert_db/sweep.sh
+++ cert_db/sweep.sh
@@ -1,3 +1,10 @@
-#! /bin/sh
+#!/bin/sh
+#
+# Copyright 2022 Broadcom. All rights reserved. The term "Broadcom"
+# refers solely to the Broadcom Inc. corporate affiliate that owns
+# the software below. This work is licensed under the OpenAFC Project License,
+# a copy of which is included with this software program
+#
+
 /usr/bin/rat-manage-api cert_id sweep --country US
 /usr/bin/rat-manage-api cert_id sweep --country CA
