7e226c88cafcaba9b5253079310f695fc2feb3c8
fs936724 <fs936724@broadcom.com>
2024-08-09 16:02:39 -0700

    119. Changes in log_utils (former rcache_common.py) - dp() got rid of keyword psarameters and does not do any formatting. But it may now print several (comma separated) values and apply special formatting (default is repr() if exists otherwise repr() if exists, othjerwise naked __dict__) - get_module_logger() now returns proxy object of LoggerProxy type. It allows to tweak logging after module start, before first use - set_parent_logger() introduced. It allows logging to piggiback Uvicorn logging

--- src/afc-packages/rcache/log_utils.py
+++ src/afc-packages/rcache/log_utils.py
@@ -1,4 +1,4 @@
-""" AFC Request cache common utility stuff """
+""" Logging-related utilities """
 #
 # Copyright (C) 2023 Broadcom. All rights reserved. The term "Broadcom"
 # refers solely to the Broadcom Inc. corporate affiliate that owns
@@ -6,51 +6,128 @@
 # a copy of which is included with this software program
 #
 
-# pylint: disable=unnecessary-pass, logging-fstring-interpolation
-# pylint: disable=global-statement
+# pylint: disable=broad-exception-caught, unnecessary-pass, global-statement
 
 import inspect
 import logging
 import os
 import sys
 import traceback
-from typing import Any, Callable, Optional, Type
+from typing import Any, Dict, Callable, List, NoReturn, Optional, Type
 import urllib.parse
+
 
 __all__ = ["dp", "error", "error_if", "FailOnError", "get_module_logger",
            "include_stack_to_error_log", "safe_dsn", "set_dp_printer",
-           "set_error_exception"]
+           "set_error_exception", "set_parent_logger"]
 
 # Exception type to raise on error()/error_if()
 _error_exception_type: type[BaseException] = SystemExit
 
 # True to include stack to log messages, created by error()/error_if()
-_include_stack_to_error_log: bool = False
+_error_include_stack: bool = False
 
 # How to print debug messages
 _dp_printer: Callable[[str], None] = lambda s: print(s, file=sys.stderr)
 
 
-def dp(*args, **kwargs) -> None:  # pylint: disable=invalid-name
+def dp(*args, to_str: Optional[Callable[[Any], str]] = None) -> None:
     """Print debug message
 
     Arguments:
-    args   -- Format and positional arguments. If latter present - formatted
-              with %
-    kwargs -- Keyword arguments. If present formatted with format()
+    args   -- One or more items to print
+    to_str -- Function for conversion values to strings
     """
-    msg = args[0] if args else ""
-    if len(args) > 1:
-        msg = msg % args[1:]
-    if args and kwargs:
-        msg = msg.format(**kwargs)
+    values: List[str] = []
+    for arg in args:
+        if to_str is not None:
+            values.append(to_str(arg))
+        elif "__repr__" in getattr(type(arg), "__dict__", {}):
+            values.append(repr(arg))
+        elif "__str__" in getattr(type(arg), "__dict__", {}):
+            values.append(str(arg))
+        elif hasattr(arg, "__dict__"):
+            values.append(str(arg.__dict__))
+        else:
+            values.append(str(arg))
     cur_frame = inspect.currentframe()
     assert (cur_frame is not None) and (cur_frame.f_back is not None)
-    frameinfo = inspect.getframeinfo(cur_frame.f_back)
-    _dp_printer(f"DP {frameinfo.function}()@{frameinfo.lineno}: {msg}")
+    caller_frameinfo = inspect.getframeinfo(cur_frame.f_back)
+    _dp_printer(
+        f"DP {caller_frameinfo.function}()@{caller_frameinfo.lineno}: "
+        f"{', '.join(values)}")
 
 
-def get_module_logger(caller_level: int = 1) -> logging.Logger:
+def set_dp_printer(dp_printer: Callable[[str], None]) -> Callable[[str], None]:
+    """ Sets new dp() printing method, returns previous one """
+    global _dp_printer
+    ret = _dp_printer
+    _dp_printer = dp_printer
+    return ret
+
+
+class LoggerProxy:
+    """ Delayer of logger instantaition until the first use
+
+    Allows to apply program/component parameters before instantiation
+
+    Private attributes:
+    _name   -- Logger name to be used at creation
+    _logger -- None or actual logger object
+
+    Private class attributes:
+    _loggers       -- Dictionary of created loggers, ordered by name
+    _parent_logger -- Optional name of parent logger
+    """
+    _loggers: Dict[str, "LoggerProxy"] = {}
+    _parent_logger = ""
+
+    def __init__(self, name: str) -> None:
+        """ Constructor
+
+        Arguments:
+        name -- Logger name
+        """
+        self._name = name
+        self._logger: Optional[logging.Logger] = None
+
+    def __getattr__(self, attr: str) -> Any:
+        """ Instantiates logger on first use and delegates to it """
+        if self._logger is None:
+            self._logger = logging.getLogger(self._name)
+        assert self._logger is not None
+        return getattr(self._logger, attr)
+
+    @classmethod
+    def set_parent_logger(cls, parent_logger: str) -> None:
+        """ Sets parent logger name """
+        cls._parent_logger = parent_logger
+
+    @classmethod
+    def get_module_logger(cls, caller_level: int) -> "LoggerProxy":
+        """ Returns logger object, named after caller module
+
+        Arguments:
+        caller_level -- How far up on stack is the caller (1 - immediate
+                        caller, etc.)
+        Returns logging.Logger object
+        """
+        caller_frame = inspect.stack()[caller_level]
+        caller_module = \
+            os.path.splitext(os.path.basename(caller_frame.filename))[0]
+        logger_name = \
+            ".".join(n for n in [cls._parent_logger, caller_module] if n)
+        if logger_name not in cls._loggers:
+            cls._loggers[logger_name] = cls(logger_name)
+        return cls._loggers[logger_name]
+
+
+def set_parent_logger(parent_logger: str) -> None:
+    """ Sets parent logger name """
+    LoggerProxy.set_parent_logger(parent_logger)
+
+
+def get_module_logger(caller_level: int = 1) -> LoggerProxy:
     """ Returns logger object, named after caller module
 
     Arguments:
@@ -58,10 +135,7 @@ def get_module_logger(caller_level: int = 1) -> logging.Logger:
                     etc.)
     Returns logging.Logger object
     """
-    caller_frame = inspect.stack()[caller_level]
-    caller_module = \
-        os.path.splitext(os.path.basename(caller_frame.filename))[0]
-    return logging.getLogger(caller_module)
+    return LoggerProxy.get_module_logger(caller_level=caller_level + 1)
 
 
 def set_error_exception(exc_type: Type[BaseException]) -> Type[BaseException]:
@@ -73,25 +147,17 @@ def set_error_exception(exc_type: Type[BaseException]) -> Type[BaseException]:
     return ret
 
 
-def set_dp_printer(dp_printer: Callable[[str], None]) -> Callable[[str], None]:
-    """ Sets new dp() printing method, returns previous one """
-    global _dp_printer
-    ret = _dp_printer
-    _dp_printer = dp_printer
-    return ret
-
-
 def include_stack_to_error_log(state: bool) -> bool:
     """ Sets if stack trace should be included into log message, generated by
     error()/error_if(). Returns previous setting
     """
-    global _include_stack_to_error_log
-    ret = _include_stack_to_error_log
-    _include_stack_to_error_log = state
+    global _error_include_stack
+    ret = _error_include_stack
+    _error_include_stack = state
     return ret
 
 
-def error(msg: str, caller_level: int = 1) -> None:
+def error(msg: str, caller_level: int = 1) -> NoReturn:
     """ Generates error exception and write it to log
 
     Arguments:
@@ -101,7 +167,7 @@ def error(msg: str, caller_level: int = 1) -> None:
     """
     stack_trace = f"Most recent call last:\n" \
         f"{''.join(traceback.format_stack()[:-caller_level])}\n" \
-        if _include_stack_to_error_log else ""
+        if _error_include_stack else ""
     get_module_logger(caller_level + 1).error(f"{stack_trace}{msg}")
     raise _error_exception_type(msg)
 
