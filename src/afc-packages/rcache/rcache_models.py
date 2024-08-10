0e6a05db907c5963c856aee996c25506a6a900d4
fs936724 <fs936724@broadcom.com>
2024-08-09 17:05:37 -0700

    119. EXCHANGE_NAME moved from rcacge_rmq.py to rcache_models.py To simplify obtaining this value from afc_server source code

--- src/afc-packages/rcache/rcache_models.py
+++ src/afc-packages/rcache/rcache_models.py
@@ -21,8 +21,14 @@ from log_utils import dp
 __all__ = ["AfcReqRespKey", "ApDbPk", "ApDbRecord", "ApDbRespState",
            "FuncSwitch", "IfDbExists", "LatLonRect", "RatapiAfcConfig",
            "RatapiRulesetIds", "RcacheClientSettings", "RcacheInvalidateReq",
-           "RcacheServiceSettings", "RcacheSpatialInvalidateReq",
-           "RcacheStatus", "RcacheUpdateReq", "RmqReqRespKey"]
+           "RCACHE_RMQ_EXCHANGE_NAME", "RcacheServiceSettings",
+           "RcacheSpatialInvalidateReq", "RcacheStatus", "RcacheUpdateReq",
+           "RmqReqRespKey"]
+
+
+# Name of RMQ exchange for delivering AFC Responses from Worker
+RCACHE_RMQ_EXCHANGE_NAME = "RcacheExchange"
+
 
 # Format of response expiration time
 RESP_EXPIRATION_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
