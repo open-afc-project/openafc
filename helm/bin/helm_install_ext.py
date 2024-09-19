5d8cafbdc5b629ebe386d544e717755fdef23eb7
fs936724 <fs936724@broadcom.com>
2024-09-18 19:45:05 -0700

    165. Changes in helm_install_*.py - --access_log replaced by --devel - Default secret store automagically selected in --fake-secrets - --print_values removed as it does not quite work

--- helm/bin/helm_install_ext.py
+++ helm/bin/helm_install_ext.py
@@ -51,8 +51,8 @@ def main(argv: List[str]) -> None:
         "--mtls", action="store_true",
         help="Enforce mTLS operation (client certificat checking)")
     argument_parser.add_argument(
-        "--access_log", action="store_true",
-        help="Enables dispatcher access log")
+        "--devel", action="store_true",
+        help="Enables development features of various containers")
     argument_parser.add_argument(
         "--wait", metavar="TIMEunit",
         help="Wait for completion for up to given timeout, specified in "
@@ -72,9 +72,6 @@ def main(argv: List[str]) -> None:
         "--set", metavar="VA.RI.AB.LE=VALUE", action="append", default=[],
         help="Additional value setting (overrides some vaslues.yaml variable)")
     argument_parser.add_argument(
-        "--print_values", choices=["yaml", "json"],
-        help="Print consolidated values in given format and exit")
-    argument_parser.add_argument(
         "RELEASE",
         help="Helm release name. 'AUTO' means construct from username and "
         "checkout directory")
@@ -84,8 +81,6 @@ def main(argv: List[str]) -> None:
         sys.exit(1)
 
     args = argument_parser.parse_args(argv)
-
-    set_silent_execute(args.print_values is not None)
 
     tag: Optional[str] = \
         auto_name(kabob=False) if args.tag == AUTO_NAME else args.tag
@@ -125,7 +120,7 @@ def main(argv: List[str]) -> None:
     for cond, filename in \
             [(args.http, "values-http.yaml"),
              (args.mtls, "values-mtls.yaml"),
-             (args.access_log, "values-access_log.yaml")]:
+             (args.devel, "values-devel.yaml")]:
         if not cond:
             continue
         assert isinstance(filename, str)
@@ -149,11 +144,6 @@ def main(argv: List[str]) -> None:
     # ... timeout
     if args.wait:
         install_args += ["--wait", "--timeout", args.wait]
-
-    if args.print_values:
-        print_helm_values(helm_args=install_args,
-                          print_format=args.print_values)
-        return
 
     # Executing helm install
     execute(install_args)
