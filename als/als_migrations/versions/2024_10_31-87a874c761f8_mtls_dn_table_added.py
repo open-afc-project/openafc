e398ad3407e1f5bd4e64d195301b17c7118fdc47
FedorSherstyukBroadcom <96546105+FedorSherstyukBroadcom@users.noreply.github.com>
2024-11-13 12:07:20 -0800

    236. sa.JSONB changed to sa_pg.JSONB (#237)
    Fixing ALS Alembic upgrade crash on als_siphon startup
    
    Co-authored-by: fs936724 <fs936724@broadcom.com>

--- als/als_migrations/versions/2024_10_31-87a874c761f8_mtls_dn_table_added.py
+++ als/als_migrations/versions/2024_10_31-87a874c761f8_mtls_dn_table_added.py
@@ -19,7 +19,7 @@ depends_on = None
 def upgrade() -> None:
     op.create_table(
         "mtls_dn",
-        sa.Column("dn_json", sa.JSONB()),
+        sa.Column("dn_json", sa_pg.JSONB()),
         sa.Column("dn_text", sa.String(), index=True),
         sa.Column("dn_text_digest", sa_pg.UUID(), index=True),
         sa.Column("month_idx", sa.SmallInteger()))
