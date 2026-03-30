39d81dc143daa1e5d87394c191ee5fc1b1612ecf
Jerome Liang <zhiyong.liang@broadcom.com>
2026-03-30 11:24:31 -0700

    450. Add OIDC support for tests in webui mode, add db init check and fix response discrepancis

--- src/ratapi/ratapi/app.py
+++ src/ratapi/ratapi/app.py
@@ -144,7 +144,13 @@ def create_app(config_override=None):
         def load_user(_id):
             ''' Load user invoked from flask login
             '''
-            return User.get(_id)
+            try:
+                return User.get(_id)
+            except exc.DataError:
+                LOGGER.warning("Invalid user_id in session cookie "
+                               "(secret key mismatch?), forcing re-login")
+                db.session.rollback()
+                return None
     else:
         # Non OIDC login.
         from flask_user import UserManager
